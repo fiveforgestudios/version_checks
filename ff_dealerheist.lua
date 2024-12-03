92da4ff26f98574abaaa55bdb2858212a055def9
akaLucifer <34045997+aka-lucifer@users.noreply.github.com>
2024-12-03 18:03:36 +0000

    quotation mark edits

--- ff_dealerheist.lua
+++ ff_dealerheist.lua
@@ -1,8 +1,8 @@
 VERSION = "1.0.3"
 CHANGES = {
     "Fixed buying Wooden Planks from the heist market.",
-    "Fixed `GetPlayerCharName` error in ESX bridge.",
-    "Fixed `Progress` call in Mythic and progress finish not applying.",
+    "Fixed \"GetPlayerCharName\" error in ESX bridge.",
+    "Fixed \"Progress\" call in Mythic and progress finish not applying.",
     "Fixed Mythic inventory remove call using old inventory method.",
     "Fixed installing spyware on Mythic not working.",
     "Fixed target use plank locale not working on Mythic.",
