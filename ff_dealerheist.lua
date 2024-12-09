0fcb80eaa5d582bd9e0e3fbb9dd306e42609780b
akaLucifer <34045997+aka-lucifer@users.noreply.github.com>
2024-12-09 03:48:36 +0000

    dealerhiest v1.0.4 patch notes

--- ff_dealerheist.lua
+++ ff_dealerheist.lua
@@ -1,11 +1,6 @@
-VERSION = "1.0.3"
+VERSION = "1.0.4"
 CHANGES = {
-    "Fixed buying Wooden Planks from the heist market.",
-    "Fixed \"GetPlayerCharName\" error in ESX bridge.",
-    "Fixed \"Progress\" call in Mythic and progress finish not applying.",
-    "Fixed Mythic inventory remove call using old inventory method.",
-    "Fixed installing spyware on Mythic not working.",
-    "Fixed target use plank locale not working on Mythic.",
-    "Fixed logs sometimes erroring on player handle.",
-    "Added missing returns in server bridges."
+    "Fixed terminal minigame breaking on third call (attempt)",
+    "Optimization & performance updates to the needle minigame.",
+    "Spelling mistake in failed hotwire log."
 }
